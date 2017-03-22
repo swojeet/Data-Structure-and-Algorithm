18fb574bdcf8b9abf6ae24564df0038ac3ad683f
swojeet <swojeetkayastha@gmail.com>
2017-03-22 14:21:36 +0545

    Implement the methods of a queue

--- queues/lib/queue.rb
+++ queues/lib/queue.rb
@@ -1,3 +1,30 @@
 class Queue
   # your code here
+  def initialize
+    @array = Array.new
+  end
+
+  def enqueue(elem)
+    @array.push(elem)
+  end
+
+  def dequeue
+    #check if the array is empty before trying to remove the first element from the queue
+    return nil if @array.empty?
+    @array.shift
+  end
+
+  def peek
+    #check if the array is empty before trying to peek the first element of the queue
+    return nil if @array.empty?
+    @array[0]
+  end
+
+  def empty?
+    @array.length ==0 ? true : false
+  end
+
+  def size
+    @array.length
+  end
 end
