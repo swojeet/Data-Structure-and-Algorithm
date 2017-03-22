96698025581a1900f8879afaf01f4e184c43c6a3
swojeet <swojeetkayastha@gmail.com>
2017-03-22 14:08:55 +0545

    Implement a stack

--- stacks/lib/stack.rb
+++ stacks/lib/stack.rb
@@ -1,3 +1,26 @@
 class Stack
   # your code here
+  def initialize
+    @array = Array.new
+  end
+
+  def push(elem)
+    @array.push(elem)
+  end
+
+  def pop
+    @array.pop
+  end
+
+  def peek
+    @array[@array.length-1]
+  end
+
+  def empty?
+    @array.length == 0 ? true : false
+  end
+
+  def size
+    @array.length
+  end
 end
